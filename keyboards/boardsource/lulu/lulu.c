a3e968370682b751f6806c091579628dfab6ee9e
Alex Bertsch <me@alexberts.ch>
2023-09-21 22:09:40 -0700

    Adding new keymap for boardsource/lulu

--- keyboards/boardsource/lulu/lulu.c
+++ keyboards/boardsource/lulu/lulu.c
@@ -23,6 +23,7 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
 #endif
 
 #ifdef OLED_ENABLE
+#ifdef OLED_DEFAULT_LULU
 oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
   if (!is_keyboard_master()) {
     return OLED_ROTATION_180;
@@ -246,4 +247,5 @@ bool oled_task_kb(void) {
     }
     return false;
 }
+#endif
 #endif
